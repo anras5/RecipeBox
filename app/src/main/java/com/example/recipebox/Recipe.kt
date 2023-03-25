261d38c71c01a25fb96932d320970a1d4e194122
anras5 <anras1filip@gmail.com>
2023-03-25 21:19:27 +0100

    Add recipe list in MainActivity.kt

---
+++ app/src/main/java/com/example/recipebox/Recipe.kt
@@ -0,0 +1,8 @@
+package com.example.recipebox
+
+data class Recipe (
+    var id: Int,
+    var name: String?,
+    var timeInMinutes: Int,
+    var description: String?,
+)
---
+++ app/src/main/java/com/example/recipebox/RecipeListItem.kt
@@ -0,0 +1,7 @@
+package com.example.recipebox
+
+data class RecipeListItem(
+    val number: Int,
+    var name: String?,
+    var timeInMinutes: Int
+)
