16d8d6590c5949735d2d1b1c998bc3c5fd5a5d13
Johnybonny <szympast@gmail.com>
2023-05-06 22:11:18 +0200

    Pasek narzędzi

--- app/src/main/java/com/example/recipebox/MainActivity.kt
+++ app/src/main/java/com/example/recipebox/MainActivity.kt
@@ -1,15 +1,19 @@
 package com.example.recipebox
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.FragmentTransaction
+
 
 class MainActivity : AppCompatActivity(), Listener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val toolbar: Toolbar = findViewById<View>(R.id.toolbar) as Toolbar
+        setSupportActionBar(toolbar)
     }
 
     override fun itemClicked(id: Long) {
